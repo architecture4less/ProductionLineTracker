838117a7900139859442d7a4ce4ca89b65f5620e
Jared O'Toole <jo39toole@yahoo.com>
2019-09-20 18:22:55 -0400

    added btnAddProd click handler

--- src/me/jwotoole9141/prodsline/Controller.java
+++ src/me/jwotoole9141/prodsline/Controller.java
@@ -1,4 +1,16 @@
 package me.jwotoole9141.prodsline;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button btnAddProd;
+
+    @FXML
+    void addProduct(ActionEvent event) {
+        System.out.println("Button clicked!");
+    }
 }
