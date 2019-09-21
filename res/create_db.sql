334093e61eec65da7471e5d7f7e53c7a67ce061a
Jared O'Toole <jo39toole@yahoo.com>
2019-09-21 01:55:54 -0400

    database creation statements

---
+++ res/create_db.sql
@@ -0,0 +1,22 @@
+create table Product
+(
+    id int auto_increment,
+    name varchar,
+    type varchar,
+    manufacturer varchar
+);
+
+create unique index Product_id_uindex
+    on Product (id);
+
+alter table Product
+    add constraint Product_pk
+        primary key (id);
+
+create table ProductionRecord
+(
+    production_num int auto_increment,
+    product_id int,
+    serial_num varchar,
+    date_produced datetime
+);
