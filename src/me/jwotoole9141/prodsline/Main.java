b82dc62e1a7aa6277b0cf44a9e327bca3a805a58
Jared O'Toole <jo39toole@yahoo.com>
2019-09-21 21:31:11 -0400

    added database

--- src/me/jwotoole9141/prodsline/Main.java
+++ src/me/jwotoole9141/prodsline/Main.java
@@ -1,3 +1,12 @@
+/*
+AUTH: Jared O'Toole
+DATE: Wed, Sep 11th, 2019
+PROJ: ProductionLineTracker
+FILE: Main.java
+
+Defines the Main class.
+ */
+
 package me.jwotoole9141.prodsline;
 
 import javafx.application.Application;
@@ -6,19 +15,57 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The main class of the Production Line Tracker application.
+ *
+ * @author Jared O'Toole
+ */
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  /**
+   * Launch the application.
+   *
+   * @param args Command-line arguments.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+  /**
+   * The starting point of the application.
+   *
+   * <p>
+   * Creates the GUI window and connects to the database.
+   * </p>
+   *
+   * @param primaryStage The root JavaFX container.
+   * @throws Exception The fxml file can't be loaded.
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+    // connect to the database...
+    Model.open();
 
-        primaryStage.setTitle("Production Line Tracker");
-        primaryStage.setScene(new Scene(root, 350, 450));
-        primaryStage.show();
-    }
+    // create the gui window...
+    Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+
+    primaryStage.setTitle("Production Line Tracker");
+    primaryStage.setScene(new Scene(root, 350, 450));
+    primaryStage.show();
+  }
+
+  /**
+   * The ending point of the application.
+   *
+   * <p>
+   * Disconnects from the database.
+   * </p>
+   */
+  @Override
+  public void stop() {
+
+    // close the database...
+    Model.close();
+  }
 }
