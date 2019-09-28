e3c490d1f7cbd46d1fc3577891775743398c6aeb
Jared O'Toole <jo39toole@yahoo.com>
2019-09-28 01:54:49 -0400

    added item interface

---
+++ src/me/jwotoole9141/prodsline/Item.java
@@ -0,0 +1,46 @@
+package me.jwotoole9141.prodsline;
+
+/**
+ * 
+ * An interface for an item with an id, name, and manufacturer.
+ * 
+ * @author Jared O'Toole
+ */
+public interface Item {
+
+  /**
+   * Get the identification number of the item.
+   * 
+   * @return The ID number.
+   */
+  int getId();
+
+  /**
+   * Get the name of this item.
+   * 
+   * @return The item name.
+   */
+  String getName();
+
+  /**
+   * Get manufacturer name of this item.
+   * 
+   * @return The manufacturer name.
+   */
+  String getManufacturer();
+
+  /**
+   * Set the name of this item.
+   *
+   * @param name The item name.
+   */
+  void setName(String name);
+
+  /**
+   * Set the manufacturer name of this item.
+   *
+   * @param manufacturer The manufacturer name.
+   */
+  void setManufacturer(String manufacturer);
+
+}
