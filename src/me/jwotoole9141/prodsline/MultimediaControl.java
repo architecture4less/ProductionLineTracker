a87c9015a6b2cd591c6ba1749b1b3197942ee2f3
Jared O'Toole <jo39toole@yahoo.com>
2019-10-08 14:46:21 -0400

    added multimedia control interface

---
+++ src/me/jwotoole9141/prodsline/MultimediaControl.java
@@ -0,0 +1,21 @@
+/*
+AUTH: Jared O'Toole
+DATE: Tue, Oct 8th, 2019
+PROJ: ProductionLineTracker
+FILE: MultimediaControl.java
+
+Defines the MultimediaControl interface.
+ */
+
+package me.jwotoole9141.prodsline;
+
+public interface MultimediaControl {
+
+  void play();
+
+  void stop();
+
+  void previous();
+
+  void next();
+}
