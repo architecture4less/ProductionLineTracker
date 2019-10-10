c95e9ab21db99a72d16a9f73c0b448276d03b248
Jared O'Toole <jo39toole@yahoo.com>
2019-10-10 16:07:36 -0400

    added movie player class

---
+++ src/me/jwotoole9141/prodsline/MoviePlayer.java
@@ -0,0 +1,89 @@
+/*
+AUTH: Jared O'Toole
+DATE: Thu, Oct 10th, 2019
+PROJ: ProductionLineTracker
+FILE: MoviePlayer.java
+
+Defines the MoviePlayer class.
+ */
+
+package me.jwotoole9141.prodsline;
+
+/**
+ * A representation of a movie player product with multimedia controls.
+ *
+ * @author Jared O'Toole
+ */
+public class MoviePlayer extends Product implements MultimediaControl {
+
+  /**
+   * The screen.
+   */
+  private Screen screen;
+
+  /**
+   * The monitor type.
+   */
+  private MonitorType monitorType;
+
+  /**
+   * Create a movie player with the given name, manufacturer, screen, and monitor type.
+   *
+   * @param name         the display name
+   * @param manufacturer the manufacturer name
+   * @param screen       the screen
+   * @param monitorType  the monitor type
+   */
+  public MoviePlayer(String name, String manufacturer, Screen screen, MonitorType monitorType) {
+
+    super(name);
+    type = ItemType.VISUAL.name();
+
+    this.manufacturer = manufacturer;
+    this.screen = screen;
+    this.monitorType = monitorType;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void play() {
+    System.out.println("Playing movie");  // temp
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void stop() {
+    System.out.println("Stopping movie");  // temp
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void previous() {
+    System.out.println("Previous movie");  // temp
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void next() {
+    System.out.println("Next movie");  // temp
+  }
+
+  /**
+   * Get a description string for this movie player.
+   *
+   * @return a multi-line description
+   */
+  @Override
+  public String toString() {
+    return super.toString() + "\nScreen: \n" + screen.toString()
+        + "\nMonitor Type: " + monitorType.name();
+  }
+}
