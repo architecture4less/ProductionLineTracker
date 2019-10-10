b1cce32484f4e23ba93ac874377286fea05c08bc
Jared O'Toole <jo39toole@yahoo.com>
2019-10-10 16:07:21 -0400

    added monitor type enum

---
+++ src/me/jwotoole9141/prodsline/MonitorType.java
@@ -0,0 +1,19 @@
+/*
+AUTH: Jared O'Toole
+DATE: Thu, Oct 10th, 2019
+PROJ: ProductionLineTracker
+FILE: MonitorType.java
+
+Defines the MonitorType enum.
+ */
+
+package me.jwotoole9141.prodsline;
+
+/**
+ * An enumeration of the monitor types of visual products.
+ *
+ * @author Jared O'Toole
+ */
+public enum MonitorType {
+  LCD, LED;
+}
