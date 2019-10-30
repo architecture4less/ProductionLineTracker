662fceba642eb831da3c29456679360a72eabed3
Jared O'Toole <jo39toole@yahoo.com>
2019-10-30 15:14:04 -0400

    added button feedback labels

--- src/me/jwotoole9141/prodsline/gui/Controller.java
+++ src/me/jwotoole9141/prodsline/gui/Controller.java
@@ -14,12 +14,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import me.jwotoole9141.prodsline.items.ItemType;
 import me.jwotoole9141.prodsline.Model;
+import me.jwotoole9141.prodsline.items.Product;
 
 /**
  * Handles user interaction with the GUI.
@@ -44,13 +46,20 @@ public class Controller {
    * The 'new product type' choice box.
    */
   @FXML
-  private ChoiceBox<String> chbNewProdType;
+  private ChoiceBox<ItemType> chbNewProdType;
 
   /**
    * The 'add product' button.
    */
   @FXML
   private Button btnAddProd;
+
+
+  /**
+   * The 'add product' error message label.
+   */
+  @FXML
+  private Label lblErrMsgAddProd;
 
   /**
    * The 'products' table.
@@ -62,13 +71,28 @@ public class Controller {
    * The 'production options' list.
    */
   @FXML
-  private ListView<?> lstProdsOpts;
+  private ListView<Product> lstProdOpts;
 
   /**
    * The 'production quantity' combo box.
    */
   @FXML
-  private ComboBox<Integer> cboProdsQnty;
+  private ComboBox<String> cboProdQnty;
+  // NOTE: the javaFX combo box returns a string
+  // for getValue(), even when its type is integer.
+
+  /**
+   * The 'produce' button.
+   */
+  @FXML
+  private Button btnProduce;
+
+
+  /**
+   * The 'produce' error message label.
+   */
+  @FXML
+  private Label lblErrMsgProduce;
 
   /**
    * The 'production log' text area.
@@ -86,15 +110,15 @@ public class Controller {
 
     for (int i = 0; i < 10; i++) {
 
-      cboProdsQnty.getItems().add(i + 1);
+      cboProdQnty.getItems().add(String.valueOf(i + 1));
     }
-    cboProdsQnty.setEditable(true);
-    cboProdsQnty.getSelectionModel().selectFirst();
+    cboProdQnty.setEditable(true);
+    cboProdQnty.getSelectionModel().selectFirst();
 
     // initialize the 'item type' choice box...
 
     for (ItemType type : ItemType.values()) {
-      chbNewProdType.getItems().add(type.getDisplay());
+      chbNewProdType.getItems().add(type);
     }
     chbNewProdType.getSelectionModel().selectFirst();
   }
@@ -110,15 +134,29 @@ public class Controller {
     System.out.println("The 'Add Product' button was clicked!");
 
     // add product to database using form info...
+    ItemType type = chbNewProdType.getValue();
+    String manuf = fldNewProdManuf.getText().trim();
+    String name = fldNewProdName.getText().trim();
+
+    Model.addProduct(name, type, manuf);
+  }
+
+
+  @FXML
+  void btnProduceAction(ActionEvent event) {
+
+    System.out.println("The 'Produce' button was clicked!");
+
+    // produce item using form info...
+    Product prod = lstProdOpts.getSelectionModel().getSelectedItem();
+    int qnty = 0;
     try {
-      ItemType type = ItemType.getFromDisplay(chbNewProdType.getValue());  // throws ex
-      String manuf = fldNewProdManuf.getText().trim();
-      String name = fldNewProdName.getText().trim();
-
-      Model.addProduct(type, manuf, name);
-
-    } catch (IllegalArgumentException ex) {
-      ex.printStackTrace();
+      qnty = Integer.parseInt(cboProdQnty.getValue());
+    } catch (NumberFormatException ignored) {
     }
+
+    System.out.printf("qnty: %d\nprod:\n%s\n\n", qnty, prod);
+
+    Model.recordProduction(prod, qnty);
   }
 }
