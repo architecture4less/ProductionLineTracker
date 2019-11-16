4d31a80ce27c33a9fbce3ef01e3a8292ff207c68
Jared O'Toole <jo39toole@yahoo.com>
2019-11-15 20:39:05 -0500

    fixed typo

--- test/me/jwotoole9141/prodsline/tests/TestDb.java
+++ test/me/jwotoole9141/prodsline/tests/TestDb.java
@@ -2,12 +2,12 @@ package me.jwotoole9141.prodsline.tests;
 
 import me.jwotoole9141.prodsline.Model;
 
-public class TestDb {
+class TestDb {
 
   public static void main(String[] args) {
 
     Model.open();
-    System.out.println(Model.getProduct(0));
+    System.out.println(Model.getProduct(1));
     Model.close();
   }
 
