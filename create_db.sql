05f9bb8120cfa0de15c0835150895c8523d26d80
Jared O'Toole <jo39toole@yahoo.com>
2019-11-23 21:19:55 -0500

    sql statement to add employee table

--- create_db.sql
+++ create_db.sql
@@ -9,6 +9,7 @@ The SQL to initialize the database.
 
 drop table if exists product;
 drop table if exists prodsrecord;
+drop table if exists employee;
 
 create table product (
     primary key (id),
@@ -28,4 +29,16 @@ create table prodsrecord (
     prodid    int       not null,
     serialnum varchar   not null,
     date      timestamp not null,
+);
+
+create table employee (
+    primary key (id),
+
+    id    int     not null auto_increment unique,
+    name  varchar not null unique,
+    first varchar as substr(trim(name), 0, position(' ', trim(name)) - 1),
+    last  varchar as substr(trim(name), position(' ', trim(name)) + 1),
+    user  varchar as concat(lower(substr(first, 0, 1)), lower(last)),
+    email varchar as concat_ws('.', lower(first), lower(last)),
+    pswd  varchar not null
 );
